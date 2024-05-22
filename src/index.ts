6fad741925dfd80d0dc2233769369a7e3e8834ea
happy2t50 <139395845+happy2t50@users.noreply.github.com>
2024-05-22 00:50:06 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { createConnection } from "typeorm";
 
 createConnection().then(async connection => {
     console.log("Connected to the database");
-
+    console.log("conection sucefull to the database");
    
 
-}).catch(error => console.log(error));
+}).catch(error => console.log(error)
+        console.log("Error al conectar la base de datos"));
